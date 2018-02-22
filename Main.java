06d96053b698690169fd6aab8de41a0f793933f4
skf152235 <skf152235@gmail.com>
2018-02-22 12:04:06 +0900

    合計処理が抜けていたので修正した

--- Main.java
+++ Main.java
@@ -5,13 +5,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int num = Integer.parseInt(sc.nextLine());
+        int sum = 0;
 
         for(int i = 1; i <=99; i++){
             if(i%num==0){
                 System.out.println(i);
+                sum += i;
             }else if(i % 10 == num){
                 System.out.println(i);
+                sum += i;
             }
         }
+        System.out.println("合計は"+sum);
     }
 }
